2b993550b53bc6c1a6b9eda98be8fce7afd3509d
felipeverones <felipe_verones@hotmail.com>
2024-04-27 22:54:28 -0300

    aula de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,30 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+        System.out.println(nomeCompleto);
+        String nomeCompletoMaior = nomeCompleto.concat(" É DEMAIS");
+        System.out.println(nomeCompletoMaior);
+
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+        //a partir do momento que detecta caracteres, faz concatenação
+
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+        //como aqui estava em parênteses, fez a soma primeiro;
+
+    }
+}
